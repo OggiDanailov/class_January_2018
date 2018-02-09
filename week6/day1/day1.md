f1e22560ca291aef6962b42eb9204ac8fec397bd
Oggi Danailov <ogidan@abv.bg>
2018-02-08 23:52:54 -0500

    week6 day1-search filter updated

--- week6/day1/day1.md
+++ week6/day1/day1.md
@@ -2,3 +2,5 @@ Intro to Database
 https://www.khanacademy.org/computing/computer-programming/sql/sql-basics/v/welcome-to-sql
 
 people.pluralize
+
+Model - 
--- week6/day1/sinatra-ar/app.rb
+++ week6/day1/sinatra-ar/app.rb
@@ -6,4 +6,19 @@ set :database, "sqlite3:textdb.sqlite3"
 require './models'
 
 
+get '/'  do 
+	@blogs = Blog.all
+	p params
+	p params[:search]
+	p params[:captures]
+
+erb :index	
+end
+
+post '/new_blog' do  
+	Blog.create(title: params[:title], category: params[:category], content: params[:content])
+	redirect '/'
+end
+
+
 
