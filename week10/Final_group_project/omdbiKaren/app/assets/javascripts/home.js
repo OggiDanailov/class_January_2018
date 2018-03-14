bad4b7fda64cb6cec0a74e7ccce9b549bdc86c80
Oggi Danailov <ogidan@abv.bg>
2018-03-14 00:33:33 -0400

    asdfds

--- week10/Final_group_project/omdbiKaren/app/assets/javascripts/home.js
+++ week10/Final_group_project/omdbiKaren/app/assets/javascripts/home.js
@@ -1,8 +1,41 @@
-$.ajax({url: "http://www.omdbapi.com/?apikey=660a106a&",
+var whatever;
+
+$.ajax({
+	url: "http://www.omdbapi.com/?apikey=660a106a&",
 					data: { s: 'love'  },   
 					dataType: "json",   
 					success: function(response){
-						var a = response.Search[0]
-						console.log(a)	
+						a = response.Search[0]
+						whatever = function(){
+							return a
+						}
+						
 					}
+
 		})
+
+
+ 
+ $('#apply').live('click', function(event, data, status, xhr) {
+    event.preventDefault();
+    return $.ajax({
+      url: '/posts/set_id',
+      type: 'GET',
+      data: {
+        code: $('#user_id').val()
+      },
+      success: function(data, event, status, xhr) {
+        $('#id_message').html(response);
+        return $("#id_message").show();
+      },
+      error: function(event, data, status, xhr) {
+        $('#id_message').html(response);
+        return $("#id_message").show();
+      }
+    });
+  });
+  
+
+
+
+
