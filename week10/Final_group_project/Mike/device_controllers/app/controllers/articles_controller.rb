0addff9d507bed58918e710171b6c5d3c140c6f4
Oggi Danailov <ogidan@abv.bg>
2018-03-18 20:54:51 -0400

    mike issue of adding content fixed

--- week10/Final_group_project/Mike/device_controllers/app/controllers/articles_controller.rb
+++ week10/Final_group_project/Mike/device_controllers/app/controllers/articles_controller.rb
@@ -17,18 +17,32 @@ class ArticlesController < ApplicationController
   end
 
   def show
-  	@article = Article.find(params[:id])
+  	@whatever = Article.find(params[:id])
   	@user = current_user
   end
 
+  # def update
+  #   puts params[:article][:content]
+  #   puts 'This is the content'
+  # 	article = Article.find(params[:id])
+  # 	article = Article.new
+  # 	if article.update(article_params)
+  # 		redirect_to "/articles/#{article.id}"
+  # 	else 
+  # 		render "/"
+  # 	end
+  # end
+
+
   def update
-  	article = Article.find(params[:id])
-  	article = Article.new
-  	if article.update(article_params)
-  		redirect_to "/articles/#{article.id}"
-  	else 
-  		render "/"
-  	end
+    
+    article = Article.find(params[:id])
+    article.content << params[:article][:content]
+    if article.save
+      redirect_to "/articles/#{article.id}"
+    else 
+      render "/"
+    end
   end
 
   private 
--- week7/day2/rails2/db/schema.rb
+++ week7/day2/rails2/db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220034138) do
+ActiveRecord::Schema.define(version: 20180223041154) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
