dbd8283f83f3118b00064c9863ee7e6ad4939af4
OggiDanailov <ogidan@abv.bg>
2018-04-26 17:46:03 -0400

    canvas game added plus challenges from steve

---
+++ canvasGame/index.js
@@ -0,0 +1,62 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var width = canvas.width;
+	var height = canvas.height;
+	var x = width/2;
+	var y = 300;
+
+// ship form
+function drawShip(){
+	ctx.beginPath();
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.rect(x, y ,50, 20);
+	ctx.fillStyle = "#FF0000";
+	ctx.fill();
+	ctx.closePath();
+}
+
+// move ship
+function moveShip(event){
+	drawShip()
+	var dx = -10;
+	var dy = 10;
+	console.log(x)
+	if(event.key == "ArrowLeft"){
+		x += dx;
+	}	
+	if(event.key == "ArrowRight"){
+		x += dy;
+	}
+	if(x >= width-50){
+		x = width-50;
+	}	
+		if(x <= 0){
+		x = 0;
+	}
+}
+
+// create bullet
+function createBullet(){
+	while(y>0){
+	ctx.beginPath()
+	ctx.clearRect(0, 0, 7, 7);
+	ctx.rect(x, y-15, 7, 7);
+	ctx.fillStyle = "blue";
+	ctx.fill();
+	ctx.closePath();
+	 y--;
+	 console.log(y)
+	}
+
+}
+
+
+document.addEventListener('keydown', function(e){
+	if(e.key == "ArrowLeft" || e.key == "ArrowRight"){
+		moveShip(e)			
+	}
+	if(e.keyCode == 32){
+		createBullet()
+	}
+})
