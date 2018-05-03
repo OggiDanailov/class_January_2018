e1a73aa15cce52f423a9b0ac6f054b8c4e63a07c
Oggi Danailov <ogidan@abv.bg>
2018-05-02 23:33:37 -0400

    week3 day4

---
+++ week3/day5/omdapi_2course/index.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+
+
+document.getElementById('submit').addEventListener("click", function(){
+	var search = document.getElementById('search').value
+	
+
+	$.ajax({url: "http://www.omdbapi.com/?apikey=660a106a&",
+		data: {s: search},
+		dataType: "json",
+		success: function (response){
+			var result = response.Search
+			console.log(result[0].Poster)
+			for(let i = 0;i<result.length;i++){
+				var titleResult = document.createElement("div")
+					titleResult.style.width = "60%"
+					titleResult.style.border = '2px solid teal'
+					titleResult.style.backgroundColor = "lightgrey"
+					titleResult.style.margin = "20px auto"
+					titleResult.style.display = 'inline-block'
+					titleResult.style.boxShadow = "10px 10px black"
+					titleResult.innerHTML += result[i].Title
+					document.body.appendChild(titleResult)
+					var poster = document.createElement('div')
+					poster.style.width = "50%"
+					poster.style.height = '400px'
+					poster.style.border = '5px solid green'
+					poster.style.boxShadow = "10px 10px black"
+					poster.style.backgroundSize = "100% 100%"
+					poster.style.backgroundImage = "url(" + result[i].Poster + ")"
+					document.body.appendChild(poster)
+					
+			}
+			
+		} 
+
+	})
+
+
+
+
+})
+
+
+
+
+
+})
---
+++ week3/extraCodingChallenges/index.js
@@ -0,0 +1,53 @@
+// write a function that takes a sentence as an argument and aways returns the penultimate word in it with 
+// .jpg attached to it.
+// Example:
+// "This is another day of my lovely life" - > lovely.jpg
+
+// function penultime(phrase){
+// 	var a = phrase.split(" ")
+// console.log(a[a.length-2] + ".jpg")
+// }
+
+// 2. write a method that gives you random names from an array in JS 
+
+// function randomize(array){
+// var a = Math.floor(Math.random() * array.length)
+
+// console.log(array)
+// }
+
+
+ // 3. Create a function that takes a number. Return true if the given  number is a multiple of 3 or 5, but not both. Example:
+// eitherNum( 6 ) -> true
+// eitherNum( 10 ) -> true
+// eitherNum( 15 ) -> false
+
+
+
+// 4.// Write a function that takes an array of arrays as an argument and returns
+ // an object literal. Instead of using the classic 'for' loop, use a 'forEach' loop.
+
+     // For example:
+//  var houseWords = [["Tully", "Family, Duty, Honor"], ["Stark", "Winter is Coming"], ["Lannister", "Hear me Roar!"], ["Martell", "Unbowed, Unbent, Unbroken"]];
+// function arrayToObj(arrayOfArrays) {   
+//     var returningObj = {};
+//     arrayOfArrays.forEach(function(innerArray) {
+//         var key = innerArray[0];
+//         var value = innerArray[1];
+//         returningObj[key] = value;
+//     });
+//     return returningObj;
+// }
+// arrayToObj(houseWords); 
+
+
+
+
+
+
+
+
+
+
+
+
