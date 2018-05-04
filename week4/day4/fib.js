ed834cf708735b29e98eaf05ab9ddae78ee06033
Oggi Danailov <ogidan@abv.bg>
2018-05-03 23:49:27 -0400

    fibonacci added without recursion

--- week4/day4/fib.js
+++ week4/day4/fib.js
@@ -6,3 +6,22 @@ return fibonacci(num - 1) + fibonacci(num - 2);
 
   
 }
+
+
+
+// without recursive
+
+function fibo(n){
+	if(n<= 1){
+		return n;
+	}
+var x = 1;
+var y = 1;
+var temp = 0;
+for(let i = 2; i<n; i++){
+	temp = x;
+	x = x + y;
+	y = temp;
+}
+	return x;
+}
