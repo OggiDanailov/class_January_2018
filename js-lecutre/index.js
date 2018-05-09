f3db067492df70b48323e692217ddfb1e1d1e39f
Oggi Danailov <ogidan@abv.bg>
2018-05-09 08:25:21 -0400

    js-lectuire added

---
+++ js-lecutre/index.js
@@ -0,0 +1,94 @@
+// 1. window object is a global object.
+// when we call window.a in the browser you will get the same result as you were calling a.
+// 'this' keyword refers to the global object in our case.
+// you can write window.a
+// or this.a or this.b();
+// var a = "whatever";
+// function b(){
+// 	console.log('asdfasdfa')
+// }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// 2. Hoisting -
+// JS executes in two different phases:
+// - first JS stores all the variables in something like Memory Space
+// - executing phase when JS executes the code.
+// Function executes becuase the execution code itself is declared.
+// All variables be default are set to undefined. When we run the code the var a gets stored 
+// to the Memory Space but without its value;instead, it is set to be undefined. 
+// when we execute the code during the second phase JS doesn't know what the value of a is.
+// Aviod Hosting overall!!
+
+//  Hoisting - here a will be undefined; b will execute; if we remove var a then we will get 
+//  an error;
+
+b()
+
+var a = 'whatever';
+console.log(a)
+
+function b(){
+	console.log('This is BBB')
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// 3. undefined !== not defined
+// Every variable receives undefined value on the first phase of JS execution. 
+// Therefore, regardless of what value a given var has, in the beginning they are all
+// undefined as far as they are declared in the js file.
+
+// var a;
+// console.log(a)
+
+
+
+// 4. Function invocation
+// First, JS will console.log(w) as 1;then it will invoke a(); a() will console.log 2 and then
+// it will invoke b(); b will console.log undefined;finally, will console.log 'this is the last'
+
+
+// function b(){
+// var w;
+// console.log(w)
+// }
+
+// function a(){
+// 	var w = 2;
+// 	console.log(w)
+// 	b()
+// }
+
+// var w = 1;
+// console.log(w)
+// a();
+// console.log('this is the last ' + w)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js-lecutre/notes.md
@@ -0,0 +1,15 @@
+- JS - Single threaded - executed one at the time. JS behaves in a single threaded manner.
+- Synchronous - on at a time in the order.
+
+Syntax parser - a prorgram that reads your code and determines what it does and if its grammar is valid;
+
+Lexical Environment - where something sits physically in the code you wrote;
+
+Execution context - a wrapper  to help manage the code that it is running 
+
+
+
+
+
+
+
