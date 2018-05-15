d658d5854f4484daadfb40eb5969d6393a5af8ed
OggiDanailov <ogidan@abv.bg>
2018-05-15 17:49:38 -0400

    wha

--- processing.js/main.js
+++ processing.js/main.js
@@ -1,6 +1,19 @@
+var sq;
+var bullet;
+var bullets = [];
+var enemies = [];
+var speedX  = 1;
+var speedY = 4;
+var xShip;
+var yShip;
+var moving = 15
+var width = 500;
+var height = 300;
+
 function setup(){
 	createCanvas(500, 300)
-
+	sq = new Square()
+	
 }
 
 // 1. background - only for the whole createCanvas
@@ -12,46 +25,113 @@ function setup(){
 // fill
 // rect
 
-var speedX  = 1;
-var speedY = 4;
-var x = 20;
-var y = 124;
+
 
 
 function draw(){
-	// this function is looping over and over again
-background(221, 143,32)	
+	background(221, 143,32)
+// this function is looping over and over again
+	sq.draw()
+	
+	for(let i = 0;i<bullets.length;i++){
+		bullets[i].draw()
+		bullets[i].move()
 
-stroke(255, 0 , 0)
-fill(0,0, 255)
-rect(32,32,32,32)
+	}
 
-stroke(0)
-fill(133,133,133)
-rect(132,132,32,32)
+	for(let i = 0; i< 12; i++){
+		enemies.push(new Enemies)
 
-stroke(32)
-// noStroke()
-fill(233, 43, 1)
-ellipse(x, y, 23,23)
-x = x + speedX
-if (x > width || x < 0)  {
-     	speedX = -speedX;
-  	}
+	}
+	// stroke(32)
+	// // noStroke()
+	// fill(233, 43, 1)
+	// ellipse(x, y, 23,23)
+	// x = x + speedX
+	// if (x >= width || x < 0)  {
+	//      	speedX = -speedX;
+	//   	}
 
-y += speedY;
-if(y> height || y <0){
-	speedY =- speedY
-}if(y>100 && y <250){
-	y += speedY *2
+	// y += speedY;
+	// if(y>= height || y <0){
+	// 	speedY =- speedY
+
+	// }if(y> 100 && y <250){
+	// 	y += speedY *4
+	// }
+
+
+	
 }
 
 
+function Square(){
+	xShip = width/2 ;
+	yShip =  height-30;
+	this.draw = function(){
+		
+		stroke(255, 0 , 0)
+		fill(0,0, 255)
+		rect(xShip ,yShip, 32, 32)
+	}
+	this.move = function(dir){
+		if(dir == 'ArrowLeft'){
+
+			if(xShip <= 0){
+				xShip = 0
+			}	
+			xShip -=  moving;
+		}else if(dir == 'ArrowRight'){
+			if(xShip >= width-30){
+				xShip = width-30
+
+			}
+				xShip += moving;
+		}if(dir == 'ArrowUp'){
+			if(dir <= 0){
+				yShip = 0
+			}
+			yShip -= moving;
+
+		}else if(dir == "ArrowDown"){
+			yShip += moving;
+		}
+
+	}
 
 }
 
+function keyPressed(event){
+	if(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'ArrowUp' || event.key == 'ArrowDown'){
+		sq.move(event.key)
+	}
+	if(event.key == " "){
+		bullets.push(new Bullet)
+	}
 
+}
 
+function Bullet(){
+	this.x = xShip;
+	this.y = yShip;
+	this.draw = function(){
+		ellipse(this.x, this.y, 8, 8)
+		// bullets.push(new Bullet)
+		
+	}
+	this.move = function(){		
+			this.y -= 2; 		
+	}
+
+}
+
+function Enemies(){
+	this.x = 20;
+	this.y = 20;
+	this.draw = function(){
+		ellipse(this.x, this.y, 20, 20)
+	}
+}
 
 
 
