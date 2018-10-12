f2ec32b77d730c88d39b9d63034c8922d7346116
Oggi Danailov <ogidan@abv.bg>
2018-10-11 22:09:12 -0400

    some challenges

--- week4/day3/JSnewSyntax/JSnewSyntax.js
+++ week4/day3/JSnewSyntax/JSnewSyntax.js
@@ -4,39 +4,39 @@
 // // operation on each of the elements in the array and return the 
 // // same amount of items in the array, use the map.
 
-// const animals = [
-//     {
-//         "name": "cat",
-//         "size": "small",
-//         "weight": 5
-//     },
-//     {
-//         "name": "dog",
-//         "size": "small",
-//         "weight": 10
-//     },
-//     {
-//         "name": "lion",
-//         "size": "medium",
-//         "weight": 150
-//     },
-//     {
-//         "name": "elephant",
-//         "size": "big",
-//         "weight": 5000
-//     }
-// ]
+const animals = [
+    {
+        "name": "cat",
+        "size": "small",
+        "weight": 5
+    },
+    {
+        "name": "dog",
+        "size": "small",
+        "weight": 10
+    },
+    {
+        "name": "lion",
+        "size": "medium",
+        "weight": 150
+    },
+    {
+        "name": "elephant",
+        "size": "big",
+        "weight": 5000
+    }
+]
 
 
-// // animals.map(function(animal, index){
-// //     console.log(index + " " + animal.name)
+// animals.map(function(animal, index){
+//     console.log(index + " " + animal.name)
 
-// // })
+// })
 
-// // let animal_names = animals.map((animal, index) => {
-// //     return index+ 1 + " " +  animal.name + ' ' + 'is a great animal'
-// // })
-// // console.log(animal_names)
+// let animal_names = animals.map((animal, index) => {
+//     return index+ 1 + " " +  animal.name + ' ' + 'is a great animal'
+// })
+// console.log(animal_names)
 
 // // filter()
 // // If i already have an array but i only want to have items in the array that match certain criteria, use the filter. 
