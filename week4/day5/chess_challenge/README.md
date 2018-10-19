10db20f7c90bc5f57a274ef61745bc465900b614
Oggi Danailov <ogidan@abv.bg>
2018-10-18 22:12:47 -0400

    extra coding challenge done

---
+++ week4/day5/chess_challenge/README.md
@@ -0,0 +1,4 @@
+1. Create a chessboard using only JS (createElement());
+	1 1/2 create a button 'restore'
+2. When you click black chess square change the color of the chess square to red and the button to red; when you click white chess square change the color of the chess square to blue and the button to blue;
+3. when you click the button restore the original chess square colors plus the original color of the button
--- week4/day5/chess_challenge/index.js
+++ week4/day5/chess_challenge/index.js
@@ -1,12 +1,26 @@
+
 var wrapper = document.createElement('div')
-wrapper.style.border = "2px solid black"
+wrapper.style.border = "5px solid black"
 wrapper.style.width = '800px'
 wrapper.style.margin = '0 auto'
 document.body.appendChild(wrapper)
 
+var button = document.createElement('BUTTON')
+var text = document.createTextNode("Restore")
+button.appendChild(text)
+button.style.backgroundColor = 'grey';
+button.style.border = '2px solid'
+button.classList.add('button');
+document.body.appendChild(button)
+var btn = document.getElementsByClassName('button')[0]													
+
+
+
+function chess(){
 	for(let i = 0;i < 8; i++){
 		for(let j=0;j<8;j++){
 			var block = document.createElement('div')
+				block.classList.add('buttons')
 				block.style.width = '100px'
 				block.style.height='100px'
 				block.style.display = 'inline-block'
@@ -29,6 +43,53 @@ document.body.appendChild(wrapper)
 				}
 			}
 		}
-	
-
 	}
+	passFurther()
+}
+
+function passFurther(){
+	var buttons = document.getElementsByClassName('buttons')
+	for(let i =0;i<buttons.length;i++){
+		buttons[i].addEventListener('click', function(){
+			changeColor(i, buttons)
+		})
+	}	
+}
+
+function changeColor(x, buttons){
+	if(buttons[x].style.backgroundColor == 'black'){
+		buttons[x].style.backgroundColor = 'red'
+		btn.style.backgroundColor = 'red'
+	}
+	if(buttons[x].style.backgroundColor == 'grey'){
+		buttons[x].style.backgroundColor = 'blue'
+		btn.style.backgroundColor = 'blue'
+	}
+	restore(x, buttons)
+}
+
+
+
+function restore(x, buttons){
+	btn.addEventListener('click', function(){
+		btn.style.backgroundColor = 'grey'
+			if(buttons[x].style.backgroundColor == 'red'){
+				buttons[x].style.backgroundColor = 'black'	
+			}
+			if(buttons[x].style.backgroundColor == 'blue'){
+				buttons[x].style.backgroundColor = 'grey'	
+			}
+
+		})
+}
+
+
+
+window.onload = chess();
+
+
+
+
+
+
+	
