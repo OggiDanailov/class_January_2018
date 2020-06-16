742043dde64931a5613af5aed8b78ae81dbaa504
OggiDanailov <ogidan@abv.bg>
2020-06-16 17:45:06 -0400

    another update

--- UArts/semester_II/week3/lecture.js
+++ UArts/semester_II/week3/lecture.js
@@ -63,7 +63,9 @@
 //  JSON.stingify() - will convert object literal into JSON:
 
  
-//  Aplication Programming Interface. 
+//  Aplication Programming Interface.
+
+// API stands for Application Programming Interface. An API is a software intermediary that allows two applications to talk to each other. In other words, an API is the messenger that delivers your request to the provider that you're requesting it from and then delivers the response back to you. 
 
 
 
