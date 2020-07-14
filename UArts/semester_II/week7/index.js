76d366a1ee8dcf89ac9d3f3aa0c4b36a849ae474
OggiDanailovLab <oxd017@jefferson.edu>
2020-07-14 16:55:18 -0400

    week7

---
+++ UArts/semester_II/week7/index.js
@@ -0,0 +1,35 @@
+// lecture part
+
+// JSON
+// Json stands for Javascript Object Notation - this notation is used to exchage data.
+
+// var myJSON = '{"name":"John", "age":31, "city":"New York"}';
+
+// var a  = JSON.parse(myJSON);
+
+// console.log(a.name)
+// console.log(a.age)
+
+// var whatever = {
+// 	name: 'Oggi',
+// 	age: 33,
+// 	country: "Serbia"
+// }
+
+// var b = JSON.stringify(whatever)
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
